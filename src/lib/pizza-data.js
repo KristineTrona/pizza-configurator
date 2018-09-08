be20d71fc28a0727c7979a10a83b7df8ceb33317
Kristine <kristinetrona@gmail.com>
2018-09-08 09:03:50 +0200

    initial commit

---
+++ src/lib/pizza-data.js
@@ -0,0 +1,56 @@
+const base = [
+  {
+    title: "25cm NY Style",
+    price: 8.99
+  },
+  {
+    title: "30cm NY Style",
+    price: 11.49
+  },
+  {
+    title: "35cm NY Style",
+    price: 13.49
+  }
+]
+
+const sauce = [
+  {
+    title: "White sauce",
+    price: 0
+  },
+  {
+    title: "Red sauce",
+    price: 1
+  },
+  {
+    title: "Mix it up",
+    price:  1.50
+  }
+]
+
+const toppings = [
+  {
+    title: "Pineapple",
+    price: 0.50
+  },
+  {
+    title: "Corn",
+    price: 0.50
+  },
+  {
+    title: "Olives (green)",
+    price: 0.50
+  },
+  {
+    title: "Red onion",
+    price: 0.50
+  },
+  {
+    title: "Spinach",
+    price: 0.50
+  },
+  {
+    title: "Cherry tomatoes",
+    price: 0.50
+  }
+]
